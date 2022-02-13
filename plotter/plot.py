707dd137e89f93c215d82ad060fefdf940ad3cb7
Mathias Nørskov <bondnoerskov@hotmail.com>
2022-02-13 19:51:29 +0100

    Grouped bar chart

--- plotter/plot.py
+++ plotter/plot.py
@@ -67,7 +67,8 @@ class plot:
             xaxis_title=x_label,
             yaxis_title=y_label,
             legend_title=legend_title,
-            showlegend=self.show_legend
+            showlegend=self.show_legend,
+            colorway=self.colorList
         )
 
         self.fig.update_layout(layout)
@@ -103,8 +104,42 @@ class plot:
         )
         self.show_legend=False
 
-    def bar_grouped(self,x, y, group, barmode='stack', x_title=None, y_title=None, title=None):
-        pass
+    def bar_grouped(self, x:list, y:list, group:list, barmode:str='stack', x_title:str=None, y_title:str=None, title:str=None, colors:list=None, texttemplate:str=None, annotation:bool=False)->go.Figure():
+        
+        groups = list(dict.fromkeys(group))   
+        group_indexex = [{group_value: [i for i, x in enumerate(group) if x==group_value]} for group_value in groups] 
+        if colors is None:
+            colors = [None] * len(groups)
+
+        for group_value, color in zip(group_indexex, colors):
+            for group_name, index in group_value.items():
+                x_list = [x[i] for i in index]
+                y_list = [y[i] for i in index]
+                if annotation:
+                    y_text = y_list
+                else:
+                    y_text = None
+
+                self.fig.add_trace(
+                    go.Bar(
+                        x=x_list, y=y_list,
+                        text=y_text,
+                        texttemplate=texttemplate,
+                        name=group_name,
+                        marker=dict(color=color),
+
+                        # Creating pretty hover boxes
+                        customdata=[group_name]*len(y_list),
+                        hovertemplate = "%{customdata}: %{y}<extra></extra>"
+                    )
+                )
+
+        self._set_layout(x_label=x_title, y_label=y_title, title=title)
+        self.fig.update_layout(barmode=barmode)
+        if self.show_fig:
+            self.fig.show()
+
+        return self.fig
 
     def bar(self, x, y, x_title:str=None, y_title:str=None, title:str=None, annotation:bool=False, annotation_format:str=None, type:str='category')->go.Figure():
         """Creating a bar plot
@@ -184,6 +219,8 @@ class plot:
         """
         groups = list(dict.fromkeys(group))   
         group_indexex = [{group_value: [i for i, x in enumerate(group) if x==group_value]} for group_value in groups] 
+        if colors is None:
+            colors = [None] * len(groups)
 
         for group_value, color in zip(group_indexex, colors):
             for group_name, index in group_value.items():
@@ -231,13 +268,13 @@ class plot:
         
 
 def main():
-    x = [1,2]
-    y = [1,2]
+    x = [1,2,1,2]
+    y = [1,2,2,3]
     group = ['a', 'a', 'b', 'b'] 
-    colors=['red', 'blue']
-    #plot(show_fig=True).continuous_grouped(x, y, group=group, colors=colors, end_annotation=True)
+    #colors=['red', 'blue']
+    #plot(show_fig=True).continuous_grouped(x, y, group=group, end_annotation=True)
 
-    plot(show_fig=True).bar(x, y, annotation=True)
+    plot(show_fig=True).bar_grouped(x, y, group, annotation=True)
 
 if __name__=='__main__':
     main()
