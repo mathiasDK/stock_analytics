360f838ff5806be6423b19266c87cb145b3d3606
Mathias Nørskov <bondnoerskov@hotmail.com>
2022-03-15 20:58:35 +0100

    Adding kpis

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -36,10 +36,10 @@ def get_linked_tickers(ticker:str, ticker_df_in: pd.DataFrame, peer_group:int) -
 def create_peer_df(ticker:str):
     ticker_dict = {
         'ticker': [ticker],
-        'sector': [None],
-        'industry': [None],
-        'market_value': [None],
-        'currency': [None],
+        # 'sector': [None],
+        # 'industry': [None],
+        # 'market_value': [None],
+        # 'currency': [None],
         'peer_group': [0]
     }
 
@@ -49,42 +49,13 @@ def create_peer_df(ticker:str):
 
     return ticker_dataframe.drop_duplicates()
 
-def get_currency(ticker:str):
-    try:
-        url = f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com'
-        # print(url)
-        read_data = ur.urlopen(url).read()
-        soup = BeautifulSoup(read_data,'html.parser')
-        site_json=json.loads(soup.text)
-        currency = site_json['quoteSummary']['result'][0]['earnings']['financialCurrency']
-        return currency
-    except Exception as e:
-        print(e)
-        return None
-
-def get_profile(ticker:str):
-    try:
-        url = f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com'
-        read_data = ur.urlopen(url).read()
-        soup = BeautifulSoup(read_data,'html.parser')
-        site_json=json.loads(soup.text)
-
-        # Variables:
-        sector = site_json['quoteSummary']['result'][0]['summaryProfile']['sector']
-        industry = site_json['quoteSummary']['result'][0]['summaryProfile']['industry']
-        currency = site_json['quoteSummary']['result'][0]['earnings']['financialCurrency']
-        enterpriseValue = site_json['quoteSummary']['result'][0]['defaultKeyStatistics']['enterpriseValue']['raw']
-        return sector, industry, currency, enterpriseValue
-    except Exception as e:
-        print(e)
-        return None, None, None, None
-
 class stock_info:
     def __init__(self, ticker: str) -> None:
         self.ticker = ticker
         self.url = f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com'
         self._load_json()
         self.currency_conversion = 1.
+        self.denominator = 1e9
 
     def _load_json(self) -> json:
         read_data = ur.urlopen(self.url).read()
@@ -120,15 +91,18 @@ class stock_info:
         """Getting the currency from the summary of the ticker. Returns None if there is an error while loading
 
         Returns:
-            str: The industry
+            str: The currency
         """
         try:
             return self.site_json['quoteSummary']['result'][0]['earnings']['financialCurrency']
-        except Exception as e:
-            print(f'It wasn\'t possible to load the industry for {self.ticker}', e)
-            return None
+        except:
+            try: 
+                return self.site_json['quoteSummary']['result'][0]['financialData']['financialCurrency']
+            except Exception as e:
+                print(f'It wasn\'t possible to load the currency for {self.ticker}', e)
+                return None
 
-    def get_market_value(self) -> str:
+    def get_market_value(self) -> float:
         """Getting the market_value from the summary of the ticker. Returns None if there is an error while loading
         
         To do:
@@ -138,26 +112,52 @@ class stock_info:
             str: The market value
         """
         try:
-            return self.site_json['quoteSummary']['result'][0]['defaultKeyStatistics']['enterpriseValue']['raw'] * self.currency_conversion
+            return self.site_json['quoteSummary']['result'][0]['defaultKeyStatistics']['enterpriseValue']['raw'] * self.currency_conversion / self.denominator
         except:
-            print(f'It wasn\'t possible to load the industry for {self.ticker}')
+            print(f'It wasn\'t possible to load the market value for {self.ticker}')
             return None
 
+    def get_gross_margin(self) -> float:
+        """Getting the gross margin from the summary of the ticker. Returns None if there is an error while loading
 
-def test(ticker:str):
-    url = f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com'
-    print(url)
-    read_data = ur.urlopen(url).read()
-    soup = BeautifulSoup(read_data,'html.parser')
-    site_json=json.loads(soup.text)
-    for k in site_json['quoteSummary']['result'][0]:
-        print(k)#['earnings']['financialCurrency']
-    print(site_json['quoteSummary']['result'][0]['summaryProfile']['industry'])
-    print(site_json['quoteSummary']['result'][0]['summaryProfile']['sector'])
-    print(site_json['quoteSummary']['result'][0]['defaultKeyStatistics']['enterpriseValue']['raw'])
+        Returns:
+            float: The gross margin
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['financialData']['grossMargins']['raw']
+        except Exception as e:
+            print(f'It wasn\'t possible to load the gross margin for {self.ticker}', e)
+            return None    
+
+    def get_ebitda_margin(self) -> float:
+        """Getting the ebitda margin from the summary of the ticker. Returns None if there is an error while loading
+
+        Returns:
+            float: The ebitda margin
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['financialData']['ebitdaMargins']['raw']
+        except Exception as e:
+            print(f'It wasn\'t possible to load the ebitda margin for {self.ticker}', e)
+            return None
+    
+    def get_operating_margin(self) -> float:
+        """Getting the ebitda margin from the summary of the ticker. Returns None if there is an error while loading
+
+        Returns:
+            float: The ebitda margin
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['financialData']['operatingMargins']['raw']
+        except Exception as e:
+            print(f'It wasn\'t possible to load the operating margin for {self.ticker}', e)
+            return None   
 
 if __name__=='__main__':
     df = create_peer_df('LVMUY')
+    cols = ['sector', 'industry', 'currency', 'market_value', 'gross_margin', 'ebitda_margin', 'operating_margin']
+    df[cols] = None
+    
     for idx, ticker in enumerate(df.ticker.values):
         print(ticker)
         stock_info_ticker = stock_info(ticker)
@@ -166,6 +166,9 @@ if __name__=='__main__':
         df.at[idx, 'industry'] = stock_info_ticker.get_industry()
         df.at[idx, 'currency'] = stock_info_ticker.get_currency()
         df.at[idx, 'market_value'] = stock_info_ticker.get_market_value()
+        df.at[idx, 'gross_margin'] = stock_info_ticker.get_gross_margin()
+        df.at[idx, 'operating_margin'] = stock_info_ticker.get_operating_margin()
+        df.at[idx, 'ebitda_margin'] = stock_info_ticker.get_ebitda_margin()
         # df.loc[df.ticker==ticker,['sector', 'industry', 'currency']] = get_currency(ticker)
     print(df)
     # get_profile('LVMUY')
