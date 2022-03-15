75a0eb476fa544ab5ff2cdaac092b44e8189fe03
Mathias Nørskov <bondnoerskov@hotmail.com>
2022-03-15 20:20:47 +0100

    Initiating the stock_info class

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -79,6 +79,71 @@ def get_profile(ticker:str):
         print(e)
         return None, None, None, None
 
+class stock_info:
+    def __init__(self, ticker: str) -> None:
+        self.ticker = ticker
+        self.url = f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com'
+        self._load_json()
+        self.currency_conversion = 1.
+
+    def _load_json(self) -> json:
+        read_data = ur.urlopen(self.url).read()
+        soup = BeautifulSoup(read_data,'html.parser')
+        site_json=json.loads(soup.text)
+        self.site_json = site_json
+    
+    def get_sector(self) -> str:
+        """Getting the sector from the summary of the ticker. Returns None if there is an error while loading
+
+        Returns:
+            str: The sector
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['summaryProfile']['sector']
+        except:
+            print(f'It wasn\'t possible to load the sector for {self.ticker}')
+            return None
+
+    def get_industry(self) -> str:
+        """Getting the industry from the summary of the ticker. Returns None if there is an error while loading
+
+        Returns:
+            str: The industry
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['summaryProfile']['industry']
+        except:
+            print(f'It wasn\'t possible to load the industry for {self.ticker}')
+            return None
+
+    def get_currency(self) -> str:
+        """Getting the currency from the summary of the ticker. Returns None if there is an error while loading
+
+        Returns:
+            str: The industry
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['earnings']['financialCurrency']
+        except Exception as e:
+            print(f'It wasn\'t possible to load the industry for {self.ticker}', e)
+            return None
+
+    def get_market_value(self) -> str:
+        """Getting the market_value from the summary of the ticker. Returns None if there is an error while loading
+        
+        To do:
+        - Should be able to handle local currency conversion, so the numbers are comparable
+
+        Returns:
+            str: The market value
+        """
+        try:
+            return self.site_json['quoteSummary']['result'][0]['defaultKeyStatistics']['enterpriseValue']['raw'] * self.currency_conversion
+        except:
+            print(f'It wasn\'t possible to load the industry for {self.ticker}')
+            return None
+
+
 def test(ticker:str):
     url = f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com'
     print(url)
@@ -95,11 +160,12 @@ if __name__=='__main__':
     df = create_peer_df('LVMUY')
     for idx, ticker in enumerate(df.ticker.values):
         print(ticker)
-        sector, industry, currency, enterpriseValue = get_profile(ticker)
-        df.at[idx, 'sector'] = sector
-        df.at[idx, 'industry'] = industry
-        df.at[idx, 'currency'] = currency
-        df.at[idx, 'market_value'] = enterpriseValue
+        stock_info_ticker = stock_info(ticker)
+        # sector, industry, currency, enterpriseValue = get_profile(ticker)
+        df.at[idx, 'sector'] = stock_info_ticker.get_sector()
+        df.at[idx, 'industry'] = stock_info_ticker.get_industry()
+        df.at[idx, 'currency'] = stock_info_ticker.get_currency()
+        df.at[idx, 'market_value'] = stock_info_ticker.get_market_value()
         # df.loc[df.ticker==ticker,['sector', 'industry', 'currency']] = get_currency(ticker)
     print(df)
     # get_profile('LVMUY')
