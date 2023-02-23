ff6136b7ea072b0373dfabcbc9cd4175d3ad37ac
Mathias Nørskov <bondnoerskov@hotmail.com>
2023-02-23 20:42:08 +0100

    Adding tests

---
+++ tests/financial_forecast_test.py
@@ -0,0 +1,10 @@
+import unittest
+from src.financial_forecast.forecast_financials import cagr
+
+class TestGrowthMethods(unittest.TestCase):
+    
+    def test_constant_growth(self):
+        self.assertEqual(cagr(1, 4, 2), 1)
+
+if __name__ == '__main__':
+    unittest.main()
